337f62b7aea1cd0acd59498b8cf0053b2c196af1
Gabrielė Stravinskaitė <stravinskaite.g@gmail.com>
2022-01-05 17:42:36 +0100

    3_3: getting N matrix (matrix inverse and deduce from identity)

--- 3_3.py
+++ 3_3.py
@@ -11,7 +11,8 @@ The solution relies on: https://en.wikipedia.org/wiki/Absorbing_Markov_chain#Abs
     a) implement matrix inverse
     b) implement "deduce from identity" method 
     c) N: invert deduced Q from identity matrix
-
+    DONE 
+    
 3. Get B:
     a) implement matrix multiplication
     b) multiply N*R
@@ -25,16 +26,29 @@ Dealing with fractions: https://docs.python.org/2.7/library/fractions.html
 """
 
 import fractions
+
+def solution(m):
+    R,Q = split_the_matrix(m)
+
+    N = getMatrixInverse(deduce_m_from_identity(Q))
+    return N
+
 def split_the_matrix(m):
 
     terminal_states_indices = [ind for ind, row in enumerate(m) if check_if_terminal(row)]
-    transient_states = filter(lambda v: not check_if_terminal(v), m)
-    transient_states_probabilities = map(convert_to_probabilities, transient_states)
+    transient_states = [row for row in m if not check_if_terminal(row)]
+    transient_states_probabilities = [convert_to_probabilities(row) for row in transient_states]
 
     R_matrix, Q_matrix = make_R_Q_matrices(transient_states_probabilities, terminal_states_indices)
 
 
     return R_matrix, Q_matrix
+
+def deduce_m_from_identity(m):
+    for row_idx, row in enumerate(m):
+        for cell_idx, cell in enumerate(row):
+           m[row_idx][cell_idx] = -cell 
+    return m
 
 def make_R_Q_matrices(matrix, terminal_indices):
     R_matrix = []
@@ -60,6 +74,43 @@ def check_if_terminal(row):
     else:
         return False
 
+def transposeMatrix(m):
+    return map(list,zip(*m))
+
+def getMatrixMinor(m,i,j):
+    return [row[:j] + row[j+1:] for row in (m[:i]+m[i+1:])]
+
+def getMatrixDeternminant(m):
+    #base case for 2x2 matrix
+    if len(m) == 2:
+        return m[0][0]*m[1][1]-m[0][1]*m[1][0]
+
+    determinant = 0
+    for c in range(len(m)):
+        determinant += ((-1)**c)*m[0][c]*getMatrixDeternminant(getMatrixMinor(m,0,c))
+    return determinant
+
+def getMatrixInverse(m):
+    determinant = getMatrixDeternminant(m)
+    #special case for 2x2 matrix:
+    if len(m) == 2:
+        return [[m[1][1]/determinant, -1*m[0][1]/determinant],
+                [-1*m[1][0]/determinant, m[0][0]/determinant]]
+
+    #find matrix of cofactors
+    cofactors = []
+    for r in range(len(m)):
+        cofactorRow = []
+        for c in range(len(m)):
+            minor = getMatrixMinor(m,r,c)
+            cofactorRow.append(((-1)**(r+c)) * getMatrixDeternminant(minor))
+        cofactors.append(cofactorRow)
+    cofactors = transposeMatrix(cofactors)
+    for r in range(len(cofactors)):
+        for c in range(len(cofactors)):
+            cofactors[r][c] = cofactors[r][c]/determinant
+    return cofactors
+
 testM = [
   [0,1,0,0,0,1],  # s0, the initial state, goes to s1 and s5 with equal probability
   [4,0,0,3,2,0],  # s1 can become s0, s3, or s4, but with different probabilities
@@ -69,4 +120,4 @@ testM = [
   [0,0,0,0,0,0],  # s5 is terminal
 ]
 
-print(split_the_matrix(testM))
+print(solution(testM))
